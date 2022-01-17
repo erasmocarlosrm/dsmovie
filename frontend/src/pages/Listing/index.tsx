a3308e496bbbdae8c527778d6783c727c6e67488
Erasmo Carlos <erasmocarlosrm@gmail.com>
2022-01-16 22:17:21 -0300

    etados

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,23 @@
+import axios from "axios";
 import MovieCard from "components/MovieCarfd";
 import Pagination from "components/Pagination";
+import { useState, useEffect } from "react";
+import { BASE_URL } from "util/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/filmes?size=12&page=2`).then((response) => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+    });
+  }, []);
+
   return (
     <div>
+      <p>{pageNumber}</p>
       <Pagination />
       <div className="container">
         <div className="row">
